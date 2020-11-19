e2fd394750d827df37333c8bfabc99bc2215fcec
LeGor <legorpad@gmail.com>
2020-11-20 00:16:02 +0500

    Debug DEN

--- scr/jscript.js
+++ scr/jscript.js
@@ -1,17 +1,11 @@
-/*
-var people = ["Tom", "Alice", "Bob", "Sam", 'Hardy', 'huyardy'];
-for(var i in people){
-    console.log(people[i]);
-    document.write(people[i] + '</br>');
-}
-*/
+
 var let = prompt('Сколько тебе лет (допускаются участники от 18 до 25 лет):');
 console.log(let);
 
 if (let >= 18 && let <= 25)
     alert('Ты можеш учавствовать');
 
-else if (isNaN(let) != false)
+else if (!isNaN(let))
     alert('Некорректное значение, введи заново');
 
 else if (let == null || let == 0)
