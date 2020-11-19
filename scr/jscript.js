f3647644771c829ad7076f507f7457e2644a6fce
LeGor <legorpad@gmail.com>
2020-11-19 17:42:10 +0500

    OK

--- scr/jscript.js
+++ scr/jscript.js
@@ -1,20 +1,30 @@
+/*
 var let = prompt('Сколько тебе лет:');
 console.log(let);
-if (let >= 18 && let <= 25)
-    alert('Ты можеш учавствовать');
-else if (let = NaN)
-    alert('Введи заново');
-else
-    alert('Ты НЕ можеш учавствовать');
 
-/*
+    if (let >= 18 && let <= 25)
+        alert('Ты можеш учавствовать');
+    else if (let = NaN)
+        alert('Введи заново');
+    else
+        alert('Ты НЕ можеш учавствовать');
+
 switch (let) {
     case 25:
         alert('Ты можеш учавствовать');
         break;
-    
+
     case 18:
         alert('Ты НЕ можеш учавствовать');
         break;
 }
+
+var people = ["Tom", "Alice", "Bob", "Sam", 'Hardy', 'huyardy'];
+for(var i in people){
+    console.log(people[i]);
+}
+
+var people = ["Tom", "Bob", "Sam"];
+for (var i of people)
+    console.log(i);
 */
