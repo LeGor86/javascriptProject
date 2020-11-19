289ce0023364346a2355f79ca00221cf6ded9481
LeGor <legorpad@gmail.com>
2020-11-19 21:00:40 +0500

    Yo

--- scr/jscript.js
+++ scr/jscript.js
@@ -1,44 +1,21 @@
 /*
-var let = prompt('Сколько тебе лет:');
-console.log(let);
-
-    if (let >= 18 && let <= 25)
-        alert('Ты можеш учавствовать');
-    else if (let = NaN)
-        alert('Введи заново');
-    else
-        alert('Ты НЕ можеш учавствовать');
-
-switch (let) {
-    case 25:
-        alert('Ты можеш учавствовать');
-        break;
-
-    case 18:
-        alert('Ты НЕ можеш учавствовать');
-        break;
-}
-
 var people = ["Tom", "Alice", "Bob", "Sam", 'Hardy', 'huyardy'];
 for(var i in people){
     console.log(people[i]);
-}
-
-var people = ["Tom", "Bob", "Sam"];
-for (var i of people)
-    console.log(i);
-
-var people = ["Tom", "Alice", "Bob", "Sam", 'Hardy', 'huyardy'];
-var i = 0;
-while(i < people.length){
-    console.log(people[i]);
-    i++;
+    document.write(people[i] + '</br>');
 }
 */
-var x = 1;
-do{
-    console.log(x * x);
-    x++;
-}while(x < 10)
+var let = prompt('Сколько тебе лет (допускаются участники от 18 до 25 лет):');
+console.log(let);
 
+if (let >= 18 && let <= 25)
+    alert('Ты можеш учавствовать');
 
+else if (isNaN(let) != false)
+    alert('Некорректное значение, введи заново');
+
+else if (let == null || let == 0)
+    alert('не пойдет, надо ответить!');
+
+else
+    alert('Ты НЕ можеш учавствовать');
