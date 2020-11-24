42b0cd9ffa6b7533fa762b3ad3d0c2ab8101349e
LeGor86 <legorpad@gmail.com>
2020-11-24 22:09:30 +0500

    привязал к HTML кнопку

--- scr/jscript.js
+++ scr/jscript.js
@@ -1,15 +1,20 @@
 
+function xProverka() {
 var let = prompt('Сколько тебе лет (допускаются участники от 18 до 25 лет):');
 console.log(let);
 
-if (let >= 18 && let <= 25)
+if (let >= 18 && let <= 25){
     alert('Ты можеш учавствовать');
-
-else if (isNaN(let))
+}
+else if (isNaN(let)){
     alert('Некорректное значение, введи заново');
-
-else if (let == null || let == 0)
+    xProverka();
+}
+else if (let == null || let == 0){
     alert('не пойдет, надо ответить!');
-
+    xProverka();
+}
 else
     alert('Ты НЕ можеш учавствовать');
+
+}
